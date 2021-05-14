a797ca5749d7156d386ef10d18863862b72f82f5
Christof Jens <christofjens@gmail.com>
2021-05-14 16:11:28 +0200

    Add toggle main

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,6 +4,10 @@ import './Card.css'
 function Card() {
   const url = 'https://rickandmortyapi.com/api/character/?page=1'
   const [characters, setCharacters] = useState([])
+  const [isHidden, setIsHidden] = useState(false)
+  const toggleMain = () => {
+    setIsHidden(!isHidden)
+  }
 
   useEffect(() => {
     fetch(url)
@@ -17,10 +21,14 @@ function Card() {
         <div key={el.id} className="Card">
           <img className="Card__profile" src={el.image} alt=""></img>
           <h2 className="Card__h2">{el.name}</h2>
-          <button className="Card__button">Show Info</button>
-          <main className="Card__main">
-            {el.name} is a {el.gender} from the Series Rick and Morty.
-          </main>
+          <button className="Card__button" onClick={toggleMain}>
+            {isHidden ? 'Hide' : 'Show'} Info
+          </button>
+          {isHidden && (
+            <main className={isHidden ? 'Card__main' : 'Card__main hidden'}>
+              {el.name} is a {el.gender} from the Series Rick and Morty.
+            </main>
+          )}
         </div>
       ))}
     </>
