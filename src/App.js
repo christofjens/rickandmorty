e7368ef2b67b8b2c27205be39869a356fbaec26c
Christof Jens <christofjens@gmail.com>
2021-05-15 22:12:41 +0200

    Add character card functions

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import CharacterCard from './CharacterCard'
-import LocationCard from './LocationCard'
-import EpisodeCard from './EpisodeCard'
-import Navigation from './Navigation'
+// import LocationCard from './LocationCard'
+// import EpisodeCard from './EpisodeCard'
+// import Navigation from './Navigation'
 
 export default function App() {
-  const charUrl = 'https://rickandmortyapi.com/api/character/?page=1'
-  const locUrl = 'https://rickandmortyapi.com/api/location/?page=1'
-  const epUrl = 'https://rickandmortyapi.com/api/episode/?page=1'
+  const charUrl = 'https://rickandmortyapi.com/api/character'
+  // const [locUrl] = 'https://rickandmortyapi.com/api/location/?page=1'
+  // const [epUrl] = 'https://rickandmortyapi.com/api/episode/?page=1'
   const [characters, setCharacters] = useState([])
-  const [locations, setLocations] = useState([])
-  const [episodes, setEpisodes] = useState([])
-
+  // const [locations, setLocations] = useState([])
+  // const [episodes, setEpisodes] = useState([])
+  console.log([characters])
   useEffect(() => {
     fetch(charUrl)
       .then(res => res.json())
-      .then(resBody => setCharacters(resBody.results))
+      .then(resBody => setCharacters([resBody.results]))
   }, [charUrl])
 
-  useEffect(() => {
-    fetch(locUrl)
-      .then(res => res.json())
-      .then(resBody => setLocations(resBody.results))
-  }, [locUrl])
+  // useEffect(() => {
+  //   fetch(locUrl)
+  //     .then(res => res.json())
+  //     .then(resBody => setLocations(resBody.results))
+  // }, [locUrl])
 
-  useEffect(() => {
-    fetch(epUrl)
-      .then(res => res.json())
-      .then(resBody => setEpisodes(resBody.results))
-  }, [epUrl])
+  // useEffect(() => {
+  //   fetch(epUrl)
+  //     .then(res => res.json())
+  //     .then(resBody => setEpisodes(resBody.results))
+  // }, [epUrl])
 
   return (
     <div className="App">
       <section className="CharacterCard">
-        <CharacterCard />
+        {characters.map(character => {
+          const { name, image, gender, id, location } = character
+          return (
+            <CharacterCard
+              key={id}
+              image={image}
+              gender={gender}
+              name={name}
+              location={location}
+            />
+          )
+        })}
       </section>
 
-      <section className="LocationCard">
+      {/* <section className="LocationCard">
         <LocationCard />
       </section>
 
@@ -47,7 +58,7 @@ export default function App() {
 
       <section className="Navigation">
         <Navigation />
-      </section>
+      </section> */}
     </div>
   )
 }
--- src/CharacterCard.jsx
+++ src/CharacterCard.jsx
@@ -8,20 +8,18 @@ export default function CharacterCard({ name, image, gender, id, location }) {
   }
 
   return (
-    <>
-      <div key={id}>
-        <h2>{name}</h2>
-        from Planet {location}
-        <img src={image} alt=""></img>
-        <button onClick={() => toggleMain(!isHidden)}>
-          {isHidden ? 'Hide' : 'Show'} Info
-        </button>
-        {isHidden && (
-          <div>
-            {name} is a {gender} from the Series Rick and Morty.
-          </div>
-        )}
-      </div>
-    </>
+    <div key={id}>
+      <h2>{name}</h2>
+      from Planet {location}
+      <img src={image} alt=""></img>
+      <button onClick={() => toggleMain(!isHidden)}>
+        {isHidden ? 'Hide' : 'Show'} Info
+      </button>
+      {isHidden && (
+        <div>
+          {name} is a {gender} from the Series Rick and Morty.
+        </div>
+      )}
+    </div>
   )
 }
