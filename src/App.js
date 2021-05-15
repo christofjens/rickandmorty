cd24d66cf537b75fd438482c123ed98b26cfd4ee
Christof Jens <christofjens@gmail.com>
2021-05-15 14:26:27 +0200

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import * as React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
-import Card from './Card'
+import CharacterCard from './CharacterCard'
+import LocationCard from './LocationCard'
+import EpisodeCard from './EpisodeCard'
+import Navigation from './Navigation'
 
-function App() {
+export default function App() {
+  const charUrl = 'https://rickandmortyapi.com/api/character/?page=1'
+  const locUrl = 'https://rickandmortyapi.com/api/location/?page=1'
+  const epUrl = 'https://rickandmortyapi.com/api/episode/?page=1'
+  const [characters, setCharacters] = useState([])
+  const [locations, setLocations] = useState([])
+  const [episodes, setEpisodes] = useState([])
+
+  useEffect(() => {
+    fetch(charUrl)
+      .then(res => res.json())
+      .then(resBody => setCharacters(resBody.results))
+  }, [charUrl])
+
+  useEffect(() => {
+    fetch(locUrl)
+      .then(res => res.json())
+      .then(resBody => setLocations(resBody.results))
+  }, [locUrl])
+
+  useEffect(() => {
+    fetch(epUrl)
+      .then(res => res.json())
+      .then(resBody => setEpisodes(resBody.results))
+  }, [epUrl])
+
   return (
     <div className="App">
-      <Card />
+      <section className="CharacterCard">
+        <CharacterCard />
+      </section>
+
+      <section className="LocationCard">
+        <LocationCard />
+      </section>
+
+      <section className="EpisodeCard">
+        <EpisodeCard />
+      </section>
+
+      <section className="Navigation">
+        <Navigation />
+      </section>
     </div>
   )
 }
-
-export default App
